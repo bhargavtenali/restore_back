e6e848bdf9f91a3b28dcfdb0d250b57b899c3ce6
Bhargav Tenali <t.bhargav@kayrros.com>
2023-12-30 22:13:00 +0530

    second commit

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require("path");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-app.use(cors());
 let database;
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const initializeDBandServer = async () => {
   try {
@@ -17,6 +20,20 @@ const initializeDBandServer = async () => {
       filename: path.join(__dirname, "restore.db"),
       driver: sqlite3.Database,
     });
+
+    // Create the 'user' table if it doesn't exist
+    const createTableResult = await database.run(`
+      CREATE TABLE IF NOT EXISTS user (
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+      );
+    `);
+    if (createTableResult.changes > 0) {
+      console.log("Table 'user' created successfully.");
+    } else {
+      console.log("Table 'user' already exists.");
+    }
+
     app.listen(3000, () => {
       console.log("Restore back is running on http://localhost:3000/");
     });
@@ -27,25 +44,26 @@ const initializeDBandServer = async () => {
 };
 
 initializeDBandServer();
-const salt = crypto.randomBytes(16).toString("hex");
 
 //ap1 register user
 app.post("/register/", async (request, response) => {
-  const { username, password, name, gender } = request.body;
+  const { username, password, secret } = request.body;
+  if (!secret || secret !== process.env.SECRET) {
+    return response.status(400).send("Invalid Secret");
+  }
   const checkUser = `select username from user where username='${username}';`;
   const dbUser = await database.get(checkUser);
   console.log(dbUser);
   if (dbUser !== undefined) {
-    response.status(400);
-    response.send("User already exists");
+    response.status(400).send("User already exists");
   } else {
     if (password.length < 6) {
       response.status(400);
       response.send("Password is too short");
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
-      const requestQuery = `insert into user(name, username, password, gender) values(
-            '${name}','${username}','${hashedPassword}','${gender}');`;
+      const requestQuery = `insert into user(username, password) values(
+        '${username}','${hashedPassword}');`;
       await database.run(requestQuery);
       response.status(200);
       response.send("User created successfully");
@@ -62,8 +80,13 @@ app.post("/login/", async (request, response) => {
     const checkPassword = await bcrypt.compare(password, dbUserExist.password);
     if (checkPassword === true) {
       const payload = { username: username };
-      const jwtToken = jwt.sign(payload, "secret_key", { algorithm: "RS256" });
-      response.send({ jwtToken });
+      const jwtToken = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: "1d",
+      });
+      response
+        .cookie("access_token", jwtToken, { httpOnly: true })
+        .status(200)
+        .json({ username: username });
     } else {
       response.status(400);
       response.send("Invalid password");
@@ -76,33 +99,66 @@ app.post("/login/", async (request, response) => {
 
 //authentication jwt token
 const authenticationToken = (request, response, next) => {
-  let jwtToken;
-  const authHeader = request.headers["authorization"];
-  if (authHeader !== undefined) {
-    jwtToken = authHeader.split(" ")[1];
-  } else {
-    response.status(401);
-    response.send("Invalid JWT Token");
+  const jwtToken = request.cookies.access_token;
+  if (!jwtToken) {
+    return response.status(401).send("Invalid JWT Token");
   }
-
   if (jwtToken !== undefined) {
-    jwt.verify(
-      jwtToken,
-      "secret_key",
-      { algorithms: ["RS256"] },
-      async (error, payload) => {
-        if (error) {
-          response.status(401);
-          response.send("Invalid JWT Token");
-        } else {
-          request.username = payload.username;
-          next();
-        }
+    jwt.verify(jwtToken, process.env.SECRET, async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        next();
       }
-    );
+    });
   }
 };
 
-app.get("/restore", authenticationToken, (req, res) => {
-  res.send("Hello World!");
+app.get("/restore", authenticationToken, async (request, response) => {
+  const imageUrl = request.body.imageUrl;
+  // POST request to Replicate to start the image restoration generation process
+  let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + process.env.REPLICATE_API_KEY,
+    },
+    body: JSON.stringify({
+      version:
+        "9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3",
+      input: { img: imageUrl, version: "v1.4", scale: 2 },
+    }),
+  });
+
+  let jsonStartResponse = await startResponse.json();
+  let endpointUrl = jsonStartResponse.urls.get;
+
+  // GET request to get the status of the image restoration process & return the result when it's ready
+  let restoredImage = null;
+  while (!restoredImage) {
+    // Loop in 1s intervals until the alt text is ready
+    console.log("polling for result...");
+    let finalResponse = await fetch(endpointUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + process.env.REPLICATE_API_KEY,
+      },
+    });
+    let jsonFinalResponse = await finalResponse.json();
+
+    if (jsonFinalResponse.status === "succeeded") {
+      restoredImage = jsonFinalResponse.output;
+    } else if (jsonFinalResponse.status === "failed") {
+      response.status(400).json("Failed to restore image");
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+  response
+    .status(200)
+    .json(restoredImage ? restoredImage : "Failed to restore image");
 });
