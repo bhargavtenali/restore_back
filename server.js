1e14df595c362cdfa2038ea8781cac6d4240df4c
Bhargav Tenali <t.bhargav@kayrros.com>
2023-12-31 01:51:33 +0530

    regex added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 let database;
 const app = express();
 const corsOptions = {
-  origin: ["https://restorephotos.netlify.app/", "http://localhost:3000"], // Add localhost as an allowed origin
+  origin: [/^https:\/\/restorephotos\.netlify\.app\/.*/, /^http:\/\/localhost/], // Add localhost as an allowed origin
   methods: "GET,POST",
 };
 app.use(cors(corsOptions));
